2dfb5888b453c401c0bddd58a63780875065a7b5
Felipe Morales-Osorio <felipe.morales.osorio@gmail.com>
2023-05-12 21:04:21 -0400

    xor network added to sample tests

--- sdf/deep-learning/example-network.scm
+++ sdf/deep-learning/example-network.scm
@@ -14,7 +14,7 @@
 
 (define simple-ys #(10.0))
 
-(define revs 5000)
+(define revs 2000)
 
 (define learning-rate 0.001)
 
@@ -29,17 +29,22 @@
 
 (numerize (((l2-loss simple-target) simple-xs simple-ys) learned-theta))
 
+
+
+;;;=============================================
+;;; Training Network to Learn XOR function
+;;;=============================================
 (define xor-net
   (stack-blocks
    (list
     (dense-block 2 3)
-    (dense-block 3 3)
-    (dense-block 3 1))))
+    (dense-block 3 2)
+    (dense-block 2 1))))
 
 (define xor-target
   (block-fn xor-net))
 
-(define xor-theta
+(define xor-init-theta
   (init-theta (block-ls xor-net)))
 
 (define xor-xs
@@ -56,20 +61,17 @@
    1
    0))
 
-(define revs 2000)
+(numerize xor-init-theta)
+(numerize ((xor-target (tensor 1 0)) xor-init-theta)) 
+(numerize (((l2-loss xor-target) xor-xs xor-ys) xor-init-theta)) 
+
+(define revs 3000)
 
 (define learning-rate 0.01)
 
-(define learned-xor-theta
-  (gradient-descent ((l2-loss xor-target) xor-xs xor-ys) xor-theta))
+(define xor-learned-theta
+  (gradient-descent ((l2-loss xor-target) xor-xs xor-ys) xor-init-theta))
 
-(numerize learned-xor-theta)
-
-(numerize ((xor-target #(0 0)) xor-theta))
-
-(numerize ((xor-target #(0 1)) learned-xor-theta))
-
-(numerize (((l2-loss xor-target) xor-xs xor-ys) xor-theta))
-
-(numerize (((l2-loss xor-target) xor-xs xor-ys) learned-xor-theta))
-
+(numerize xor-learned-theta) 
+(numerize ((xor-target (tensor 1 0)) xor-learned-theta)) 
+(numerize (((l2-loss xor-target) xor-xs xor-ys) xor-learned-theta)) 
